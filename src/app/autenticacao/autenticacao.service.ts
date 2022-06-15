9e683da4770de86a230047a0d1da4f2c7412894e
David Wilson de Jesus <davidwj15@gmail.com>
2022-06-15 14:48:24 -0300

    Adicionado modulo e serviço de autenticação. Consumindo api de login.

---
+++ src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+  API_PATH = 'http://localhost:3000/';
+
+
+  constructor(private httpCliente: HttpClient) { }
+
+  autenticar(usuario: string, senha:string): Observable<any>{
+    return this.httpCliente.post(`${this.API_PATH}/user/login`,{
+      userName: usuario,
+      password: senha
+    });
+  }
+}
