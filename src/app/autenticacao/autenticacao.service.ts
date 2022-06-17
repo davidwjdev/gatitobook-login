2842022970ceea78d2469355b89ab3f590aabf90
David Wilson de Jesus <davidwj15@gmail.com>
2022-06-17 11:49:38 -0300

    ajustando autenticar usuario para salvar token

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,7 +1,8 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { API_PATH } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,19 @@ import { Observable } from 'rxjs';
 export class AutenticacaoService {
 
 
-  constructor(private httpCliente: HttpClient) { }
+  constructor(private httpCliente: HttpClient, private usuarioService: UsuarioService) { }
 
-  autenticar(usuario: string, senha:string): Observable<any>{
+  autenticar(usuario: string, senha:string): Observable<HttpResponse<any>>{
     return this.httpCliente.post(`${API_PATH}user/login`,{
       userName: usuario,
       password: senha
-    });
+    },
+    { observe: 'response'}
+    ).pipe(
+      tap((res) =>{
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    );
   }
 }
